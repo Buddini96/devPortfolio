85213d20dd877b689663f379953e5e3dc9e09b1d
DESHANISIRIWARDHANA <Ayeshadesh0319@gmail.com>
2025-03-20 22:17:44 +0530

    content updated

--- script.js
+++ script.js
@@ -51,3 +51,25 @@ function setLightMode() {
         icon.src = icon.getAttribute("src-light");
     });
 }
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
